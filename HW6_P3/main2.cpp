a0b1bc2d47dcdbe1986f42348f6ce023ec3b7fe8
johnthom11370 <112103129+jcthompson11370@users.noreply.github.com>
2022-09-30 18:45:27 +0000

    created output for each test case. Ran code to get answers

--- HW6_P3/main2.cpp
+++ HW6_P3/main2.cpp
@@ -3,6 +3,8 @@
 //Date: 09/27/2022
 
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
@@ -10,7 +12,11 @@ double power_Gen(double H, double Q);
 
 int main()
 {
+  cout << power_Gen(14, 20) << " Watts" << endl; // test #1
+
+  cout << power_Gen(30, 20) << " Watts" << endl; // test #2
   
+  return 0;
 }
 
 double power_Gen(double H, double Q)
@@ -21,3 +27,6 @@ double power_Gen(double H, double Q)
 
   return water_Density * Q * H * g; 
 }
+// test case #1: 2.7468e+06 Watts
+
+// test case #2: 5.886e+06 Watts
