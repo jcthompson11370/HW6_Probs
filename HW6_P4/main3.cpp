7f72421dabe9f1d053ac86e952440366b3b1c03a
johnthom11370 <112103129+jcthompson11370@users.noreply.github.com>
2022-09-30 22:24:21 +0000

    Added sign names

--- HW6_P4/main3.cpp
+++ HW6_P4/main3.cpp
@@ -16,55 +16,66 @@ int main()
 {
     Date_Month();
 
-
   switch(Date_Month())
     {
-      case 1:
+      case (21/ 3) > (20/ 4):
 
+        cout << "Aries" << endl;
         break;
 
       case 2:
 
+        cout << "Taurus" << endl;
         break;
 
       case 3:
 
+        cout << "Gemini" << endl;
         break;
 
       case 4:
 
+        cout << "Cancer" << endl;
         break;
 
       case 5:
 
+        cout << "Leo" << endl;
         break;
 
       case 6:
 
+        cout << "Virgo" << endl;
         break;
 
       case 7:
 
+        cout << "Libra" << endl;
         break;
 
       case 8:
 
+        cout << "Scorpio" << endl;
         break;
 
       case 9:
 
+        cout << "Sagittarius" << endl;
         break;
 
       case 10:
 
+        cout << "Capricorn" << endl;
         break;
 
       case 11:
 
+        cout << "Aquarius" << endl;
         break;
 
       case 12:
 
+        cout << "Pisces" << endl;
         break;
     }
   
@@ -92,5 +103,13 @@ int Date_Month()
   {
     cout << day <<" / "<< month << endl;
   }
+
+  
+
+
+
+
+  
   return 0;
+  
 }
