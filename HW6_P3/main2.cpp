08cf246afe4598b4e638d8ebc8bd1460230bb610
johnthom11370 <112103129+jcthompson11370@users.noreply.github.com>
2022-09-30 15:46:08 +0000

    created user defined function

---
+++ HW6_P3/main2.cpp
@@ -0,0 +1,20 @@
+//Programer: John Thompson
+//Description: HW6_P3
+//Date: 09/27/2022
+
+#include <iostream>
+
+using namespace std;
+
+double power_Gen(double H, double Q);
+
+int main()
+{
+  
+}
+
+double power_Gen(double H, double Q)
+{
+  const double g = 9.81; // gravity
+  
+}
